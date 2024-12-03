49a87ab1038d4352d3c15a1bcfcb081eb810cf5b
nqm314 <minhnq220804@gmail.com>
2024-12-03 07:43:02 +0700

    Update config

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "minh285714",
+  password: process.env.DB_PASSWORD || "your_password",
   database: process.env.DB_NAME || "book_store",
 });
 
